2973869d7da1cb3096d944f7d04c05643b2727f8
anoj17 <anojbudathoki17@gmail.com>
2025-08-28 20:49:35 +0545

    add login form

---
+++ lib/utils/utils.ts
@@ -0,0 +1,18 @@
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
+
+export function slugify(str: string) {
+  if (!str) return "";
+  str = str.replace(/^\s+|\s+$/g, ""); // trim leading/trailing white space
+  str = str.toLowerCase(); // convert string to lowercase
+  str = str
+    .replace(/[^a-z0-9 -]/g, "")
+    .trim() // remove any non-alphanumeric characters
+    .replace(/\s+/g, "-") // replace spaces with hyphens
+    .replace(/-+/g, "-"); // remove consecutive hyphens
+  return str;
+}
